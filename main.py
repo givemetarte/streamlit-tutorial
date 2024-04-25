0e16e58131d896c9529b12df2d418c4e4ef4417f
Haram Park <52904037+givemetarte@users.noreply.github.com>
2024-04-25 10:50:58 +0900

    setting

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import time
+
+st.title("Hello World")
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
